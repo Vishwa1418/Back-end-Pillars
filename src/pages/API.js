f8063cf628105eea6f041957a588bc79563845ff
smk627751 <smk627751@gmail.com>
2023-08-15 15:16:38 +0530

    updated files

--- src/pages/API.js
+++ src/pages/API.js
@@ -1,7 +1,6 @@
 import axios from "axios"
 
-// const url = process.env.REACT_APP_HOST
-const url = "https://flask-api-2-455d.onrender.com"
+const url = process.env.REACT_APP_HOST
 
 export const Post = async(endpoint,input) => {
     const {data} = await axios.post( url+endpoint, input)
